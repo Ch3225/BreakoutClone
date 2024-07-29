f8ba197009e29a2b476ad33dfb36a1fd6effae33
Ch3225 <49605433+Ch3225@users.noreply.github.com>
2024-07-29 18:29:41 +0900

    优化了信息传递

--- Arduino/sketch_jul20a/sketch_jul20a.ino
+++ Arduino/sketch_jul20a/sketch_jul20a.ino
@@ -1,18 +1,38 @@
 #include <Adafruit_NeoPixel.h>
+#include <TimerOne.h>
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(3, 2, NEO_RGB + NEO_KHZ800);
+int colors[3];
+int timer=0;
 void setup() {
   Serial.begin(9600);
   pixels.begin();
   pinMode(3,OUTPUT);
+  timer=0;
+
+  Timer1.initialize(500000); // 每秒触发一次
+  Timer1.attachInterrupt(bulbs);
+}
+void bulbs() {
+  // 中断执行的代码
+  if(colors[0]==colors[1]&&colors[1]==colors[2]){
+    if(timer%2==0){
+      digitalWrite(3,HIGH);
+    }else{
+      digitalWrite(3,LOW);
+    }
+  }else{
+    digitalWrite(3,LOW);
+  }
+  timer++;
 }
 void loop() {
-    if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    int colors[3];
-    sscanf(data.c_str(), "%d,%d,%d", &colors[0], &colors[1], &colors[2]);
+  if (Serial.available() > 0) {
+  String data = Serial.readStringUntil('\n');
+
+    if (sscanf(data.c_str(), "%d,%d,%d", &colors[0], &colors[1], &colors[2]) == 3) {
     // Map colors to RGB values
     for (int i = 0; i < 3; i++) {
-      switch(colors[i]) {
+      switch (colors[i]) {
         case 0:
           pixels.setPixelColor(i, pixels.Color(255, 0, 0)); // Red
           break;
@@ -27,6 +47,8 @@ void loop() {
     }
     pixels.show(); // Update the LED colors
   }
+  delay(20);
+}
 
 /*
   pixels.setPixelColor( 0, pixels.Color(0, 0, 255) );
--- Breakout/ArduinoHandler.pde
+++ Breakout/ArduinoHandler.pde
@@ -1,5 +1,5 @@
 void writeColor() {
     // Send the information to Arduino
     String colorInfo = currentColorIndex[0] + "," + currentColorIndex[1] + "," + currentColorIndex[2];
-    myPort.write(colorInfo);
+    myPort.write(colorInfo+"\n");
 }
